84f139fef3402bc62198d623009063e2345aca49
Bryan3598 <Bryan3598@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-31 05:43:11 +0000

    1.4.6 Minor update
    git-svn-id: https://plugins.svn.wordpress.org/kingkong-board/tags/1.4.6@1124149 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ admin/dashboard.php
@@ -0,0 +1,56 @@
+<?php
+function KingkongBoard(){
+?>
+
+<div class="KingkongBoard-Wrap">
+<input type="hidden" class="kingkongboard_plugins_url" value="<?php echo KINGKONGBOARD_PLUGINS_URL;?>">
+<?php
+
+  if(isset($_GET['view'])){
+    $view = sanitize_text_field($_GET['view']);
+  } else {
+    $view = '';
+  }
+
+  if(isset($_GET['id'])){
+    $board_id = sanitize_text_field($_GET['id']);
+  } else {
+    $board_id = null;
+  }
+
+  switch($view){
+    case "create" :
+      require_once 'KingkongBoard-Create.php';
+    break;
+
+    case "modify" :
+      require_once 'KingkongBoard-Modify.php';
+    break;
+
+    case "entry" :
+      require_once 'KingkongBoard-Entries.php';
+    break;
+
+    case "entry-write" :
+      require_once 'KingkongBoard-Entry-Write.php';
+    break;
+
+    case "entry-view" :
+      require_once 'KingkongBoard-Entry-View.php';
+    break;
+
+    case "entry-reply" :
+      require_once 'KingkongBoard-Entry-Reply.php';
+    break;
+
+    default :
+      require_once 'KingkongBoard-Index.php';
+    break;
+  }
+
+?>
+</div>
+<div id="fb-root"></div>
+<?php
+}
+?>
